8adb898e8cc5f5a03fc4ba506078413d976a4358
David Urbina <davidurbina.dev@gmail.com>
2024-07-26 10:23:58 -0500

    Bump version `0.8.7`

--- src/ConcurrentCallbackQueue.js
+++ src/ConcurrentCallbackQueue.js
@@ -81,7 +81,7 @@ const QueueState = {
  * queue.start();
  *
  * @author David Urbina (davidurbina.dev@gmail.com)
- * @version 0.8.0
+ * @version 0.8.7
  * @since 2023-03-23
  * @tutorial concurrent-callback-queue
  */
